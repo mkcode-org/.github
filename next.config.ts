81067640c834ad59669b2ca1cc4fc7f6f509db62
sukalov <matvey10@gmail.com>
2025-02-10 02:30:31 +0300

    build: update next config for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '',
+  assetPrefix: '',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
