73f97a1618a8a98ddb6b053a472c22fb767e1def
Anatoly <yatskovanatoly@gmail.com>
2025-02-09 19:40:56 +0300

    Init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+			keyframes: {
+				'logo-pulse': {
+					'0%': { opacity: '0' },
+					'49%': { opacity: '0' },
+					'50%': { opacity: '100' },
+				},
+			},
+			animation: {
+				'logo-pulse': 'logo-pulse 1s infinite',
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config
