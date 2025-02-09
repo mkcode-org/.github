ae63272e0f66306f775156f4f510dcbc86519dcc
Anatoly <yatskovanatoly@gmail.com>
2025-02-10 02:56:36 +0300

    Github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import Link from 'next/link'
 import '../styles/globals.css'
 
 const App = () => (
-	<div className='h-[100vh] w-full flex flex-col justify-center items-center'>
-		<p className='text-4xl'>
+	<div className='h-[100vh] w-full relative flex flex-col justify-center items-center'>
+		<p className='text-4xl select-none'>
 			mk means make<span className='animate-logo-pulse'>_</span>
 		</p>
+		<Link
+			href='https://github.com/mkcode-org'
+			className='text-2xl absolute bottom-16 underline underline-offset-2 hover:opacity-50'
+		>
+			github
+		</Link>
 	</div>
 )
 
